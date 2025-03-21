112e89de846a29b30eba50d163dab5e32ea0e53f
Kimsor1111 <angkimsor@gmail.com>
2025-03-21 23:15:55 +0700

    add the slider

--- swiper.js
+++ swiper.js
@@ -1,10 +1,9 @@
 var swiper1 = new Swiper(".mySwiper1", {
-  slidesPerView: 5,
   spaceBetween: 15,
   scrollbar: {
-    el: ".swiper-scrollbar",
+    el: ".scroll1",
     draggable: true,
-    hide: true
+    hide: true,
   },
   breakpoints: {
     0: {
@@ -13,27 +12,55 @@ var swiper1 = new Swiper(".mySwiper1", {
     768: {
       slidesPerView: 3,
     },
-    1024:{
-      slidesPerView: 4
+    1024: {
+      slidesPerView: 4,
     },
-    1280:{
-      slidesPerView: 5
-    }
+    1280: {
+      slidesPerView: 5,
+    },
   },
 });
-var swiper2 = new Swiper(".mySwiper2", {
-  slidesPerView: 5,
+var swiper1 = new Swiper(".mySwiper2", {
   spaceBetween: 15,
   scrollbar: {
-    el: ".swiper-scrollbar",
+    el: ".scroll2",
     draggable: true,
+    hide: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    1280: {
+      slidesPerView: 5,
+    },
   },
 });
-var swiper3 = new Swiper(".mySwiper3", {
-  slidesPerView: 5,
+var swiper1 = new Swiper(".mySwiper3", {
   spaceBetween: 15,
   scrollbar: {
-    el: ".swiper-scrollbar",
+    el: ".scroll3",
     draggable: true,
+    hide: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    1280: {
+      slidesPerView: 5,
+    },
   },
 });
